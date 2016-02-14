068ca8316bc0a17bf4a60cacd8a3659417d2416a
Umair <muhammad.umair49@gmail.com>
2016-02-14 23:23:10 +0000

    An update removed

--- src/myapp/index.go
+++ src/myapp/index.go
@@ -8,7 +8,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 		
 		h, _ := os.Hostname()
-		fmt.Fprintf(w, "Hi 11111111 there, I am served from %s!", h)
+		fmt.Fprintf(w, "Hi there, I am served from %s!", h)
 }
 
 func main() {
